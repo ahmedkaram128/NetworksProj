991e7d504716972d306aeebb49cf8d246709fa4e
ahmedFathy219 <ahmed.h.m.fathy@gmail.com>
2025-12-12 14:46:41 +0200

    Update app.js

--- myApp/app.js
+++ myApp/app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
